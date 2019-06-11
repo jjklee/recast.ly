996b5857c8e8e9f06c6eae327ccbf8d04ae452bc
Jeremy Bishow <jeremybishow@gmail.com>
2019-06-10 17:18:25 -0700

    videolistentries populates

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,12 +1,13 @@
 import exampleVideoData from '../data/exampleVideoData.js';
+import VideoListEntry from './VideoListEntry.js';
 
-// var VideoList = (props) => (
-//   <div className="video-list">
-//     {props.map(video => 
-//       <exampleVideoData video={video} />
-//     )}
-//   </div>
-// );
+var VideoList = (props) => (
+  <div className="video-list">
+    {exampleVideoData.map(video => 
+      <VideoListEntry video={video} />
+    )}
+  </div>
+);
 
 
 // // PropTypes tell other developers what `props` a component expects
@@ -20,14 +21,14 @@ import exampleVideoData from '../data/exampleVideoData.js';
 // // `var` declarations will only exist globally where explicitly defined.
 // export default VideoList;
 
-var VideoList = (props) => (
-  <div className="video-list">
-      <img src={exampleVideoData[0].snippet.thumbnails.default.url}>
-      <div>
-        <h5><em>{exampleVideoData[0].snippet.title}</em></h5>
-      </div>      
-  </div>
-);
+// var VideoList = (props) => (
+//   <div className="video-list">
+//       <img src={exampleVideoData[0].snippet.thumbnails.default.url}></img>
+//       <h5><em>{exampleVideoData[0].snippet.title}</em></h5>
+//   </div>
+// )
+
+
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
