36e8eec2dfd6db71c9aebcca2c7eacbd868daee3
jjklee <jjlee720@gmail.com>
2019-06-12 20:05:09 -0700

    Completes tests

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 // TODO: Render the `App` component to the DOM
 import App from './components/App.js';
-import YOUTUBE_API_KEY from './config/youtube.js'
-ReactDOM.render(<App searchYoutube={searchYoutube} APK_KEY={YOUTUBE_API_KEY}/>, document.getElementById('app'));
-
+import YOUTUBE_API_KEY from './config/youtube.js';
+ReactDOM.render(<App searchYoutube={searchYoutube} API_KEY={YOUTUBE_API_KEY}/>, document.getElementById('app'));
